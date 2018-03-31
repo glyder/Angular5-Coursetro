ae8e11a5a9f223d0c446cfb17b06348dd59854e2
glyder <georgepagotelis@hotmail.com>
2018-03-31 23:30:14 +1100

    Routing
    Next section is Services want to see the differences

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+// Added
+import { ActivatedRoute } from '@angular/router';   // subscribe (res.id)
+import { Router } from '@angular/router';           // router.navigate['']
+
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
@@ -7,9 +11,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private router: Router) {
+    this.route.params.subscribe(res => console.log(res.id));
+  }
 
   ngOnInit() {
+  }
+
+  sendMeHome() {
+    this.router.navigate(['']);
+    return false;
   }
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -23,14 +23,14 @@ import { trigger, style, transition, animate, keyframes, query, stagger } from '
         query(':enter', style({ opacity: 0 }), {optional: true}),
 
         query(':enter', stagger('300ms', [
-          animate('.2s ease-in', keyframes([
+          animate('.1s ease-in', keyframes([
             style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
             style({opacity: .5, transform: 'translateY(15px)',  offset: 0.3}),
             style({opacity: 1, transform: 'translateY(0)',     offset: 1.0}),
           ]))]), {optional: true})
           ,
         query(':leave', stagger('300ms', [
-          animate('.4s ease-out', keyframes([
+          animate('.1s ease-out', keyframes([
             style({opacity: 1, transform: 'translateY(0)', offset: 0}),
             style({opacity: .5, transform: 'translateY(15px)',  offset: 0.3}),
             style({opacity: 0, transform: 'translateY(-75%)',     offset: 1.0}),
